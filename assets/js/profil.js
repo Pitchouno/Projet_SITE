2b2d8d9adb86b3a15ecc42d1c8c921d5dcbb5602
Pitchouno <mathisphilippe60@gmail.com>
2024-06-28 10:18:04 +0200

    Add files via upload

---
+++ assets/js/profil.js
@@ -0,0 +1,8 @@
+// Tableau de profils fictifs
+const profiles = [
+    { name: 'Jean Dupont', formation: 'Développeur Web', ecole: 'ESIEE-IT,Princeton, France', statu:'En cours', date:'22/12/2003,22/12/2005' },
+    { name: 'Marie Curie', formation: 'Data analyste', ecole: 'ESIEE,Varsovie, Pologne', statu:'Terminé', date:'22/12/2003,22/12/2005' },
+    { name: 'Albert Einstein', formation: 'IA', ecole: 'ESIEE paris,Paris, États-Unis', statu: 'En cours', date:'22/12/204,22/12/2025'},
+    { name: 'Ada Lovelace', formation: 'Devops', ecole: 'coding factory,Londres, Royaume-Uni', statu:'Terminé', date:'22/12/2012,22/12/2014' },
+    // Ajoutez plus de profils ici
+];
